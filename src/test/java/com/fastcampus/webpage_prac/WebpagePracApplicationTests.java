2243a111c8db41c5d1d8395ffbbddefcf363eecb
NeedIDcard <jo3902@gmail.com>
2025-04-24 02:21:18 +0900

    Initial commit

---
+++ src/test/java/com/fastcampus/webpage_prac/WebpagePracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.webpage_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpagePracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
