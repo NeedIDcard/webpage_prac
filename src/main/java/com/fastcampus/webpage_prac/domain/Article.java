ce394bc7166147cf4e9801ba1e7a9ae73ab4e9dd
NeedIDcard <jo3902@gmail.com>
2025-04-27 20:51:32 +0900

    #8 - 도메인 코드를 엔티티로 변환
    1. 도메인 클래스<>1테이블 매칭되도록 설계했다.
    
    생성자와 'equals()', 'hashcode()' 등이 도메인 맥락에 맞게끔 디테일 하게 설정했다.

--- src/main/java/com/fastcampus/webpage_prac/domain/Article.java
+++ src/main/java/com/fastcampus/webpage_prac/domain/Article.java
@@ -1,4 +1,77 @@
 package com.fastcampus.webpage_prac.domain;
 
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createAt"),
+        @Index(columnList = "createBy"),
+})
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @Column(nullable = false) private String title;               //title, content, hashtag 만 유저 단에서 조작 가능
+    @Setter @Column(nullable = false, length = 10000) private String content;
+
+    @Setter private String hashtag;
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createAt;
+    @LastModifiedDate @Column(nullable = false)private LocalDateTime modifiedAt;
+    @CreatedBy @Column(nullable = false, length = 100) private String createBy;
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
+
+
+
+
+    protected Article() {}
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(id, article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
