19b065a3e75de3487156cf34e8df474ded9ba172
NeedIDcard <jo3902@gmail.com>
2025-05-06 01:01:22 +0900

    #16 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다.

---
+++ src/main/java/com/fastcampus/webpage_prac/controller/ArticleController.java
@@ -0,0 +1,10 @@
+package com.fastcampus.webpage_prac.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
